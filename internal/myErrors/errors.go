e167523ff43c9947d63f389cbca49e903dd75b6b
Alex-Nosov-ITMO <nosov3006@inbox.ru>
2024-06-30 08:18:58 +0600

    Fix myErrors

--- internal/myErrors/errors.go
+++ internal/myErrors/errors.go
@@ -3,16 +3,7 @@ package myErrors
 import (
 	"fmt"
 )
-/*
-var ErrorsString = map[string] string{
-	"BadRequest": errors.New("неверный запрос").Error(),
-	"InternalServerError": errors.New("ошибка сервера").Error(),
-	"Unauthorized": errors.New("не авторизован").Error(),
-	"NotFound": errors.New("не найдено").Error(),
-	"InvalidID": errors.New("невалидный идентификатор").Error(),
-	"WrongPassword": errors.New("неверный пароль, не авторизован").Error(),
-}
-*/
+
 var (
 	BadRequest = "неверный запрос"
 	InternalServerError = "ошибка сервера"
